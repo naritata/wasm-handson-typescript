62aee4d155201567088a5beb7ca36839ee42a7c0
naritata <narita.takeshi@optimind.tech>
2024-12-09 22:27:14 +0900

    feat: wasm用のエントリポイントを追加

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,9 @@
 // The entry file of your WebAssembly module.
 
 export function add(a: i32, b: i32): i32 {
-  return a + b;
+	return a + b;
+}
+
+export function _start(): i32 {
+	return add(10, 20);
 }
