29f28e6d06360cdc861a4c25a01fc14aaf861496
naritata <narita.takeshi@optimind.tech>
2024-12-09 19:33:12 +0900

    build: wasmerを使うためdevcontainer構成を調整

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,7 @@ cp .devcontainer/.bash_aliases ~/.bash_aliases
 ## ホスト側で使っていて、コンテナでも使いたいsshの設定をコピーする
 ## .devcontainer/.ssh/配下に予めconfigや秘密鍵を配置しておくこと
 cp .devcontainer/.ssh/* ~/.ssh/
+
+curl https://get.wasmer.io -sSfL | sh
+source /home/vscode/.wasmer/wasmer.sh
+wasmer --version
